ae78596fdaf7d9b73b137fc544134175ab537c31
Andrew Le <andrew@atscale.xyz>
2022-11-05 22:02:06 +0700

    feat: add modules to libra

--- lib/libra.ts
+++ lib/libra.ts
@@ -3,16 +3,25 @@ import { Client } from './client';
 import { WalletConnection } from './wallet-connection';
 import LrpProtocol from './lrp';
 import DisputeResolution from './dispute-resolution';
+import Identities from './identities';
+import Currencies from './currencies';
+import Resolvers from './resolvers';
 
 export class Libra {
+  readonly walletConnection: WalletConnection;
   readonly lrp: LrpProtocol;
   readonly ddr: DisputeResolution;
-  readonly walletConnection: WalletConnection;
+  readonly identities: Identities;
+  readonly currencies: Currencies;
+  readonly resolvers: Resolvers;
 
   constructor(config: LibraConfig) {
     const client = new Client(config);
     this.walletConnection = new WalletConnection([]);
     this.lrp = new LrpProtocol(client);
     this.ddr = new DisputeResolution(client);
+    this.identities = new Identities(client);
+    this.currencies = new Currencies(client);
+    this.resolvers = new Resolvers(client);
   }
 }
